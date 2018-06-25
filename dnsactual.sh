f7843ce81181caefd9babcf35804e4d724f19e20
Kevinesan Pillay <kevin.pillay@is.co.za>
2018-06-25 14:24:15 +0200

    Initial commit

--- dnsactual.sh
+++ dnsactual.sh
@@ -9,23 +9,29 @@
 #bash_version    :GNU bash, version 4.4.19(1)-release (x86_64-redhat-linux-gnu)
 #==============================================================================
 
- if test -f /etc/freedns/dnsactual.conf
-   then
-   CacheIP=$(cat /etc/freedns/dnsactual.conf)
- fi
- #echo $CacheIP
- CurreIP=$(wget http://freedns.afraid.org/dynamic/check.php -o /dev/null -O /dev/stdout | grep Detected | cut -d : -f 2 | cut -d '<' -
+IPFILE=/etc/freedns/dnsactual.dat
+
+if [[ ! -d /etc/freedns ]]
+then
+  mkdir -p /etc/freedns
+fi
+
+if [[ -f $IPFILE ]]
+then
+  CACHEIP=$(cat $IPFILE)
+fi
+
+CURRIP=$(wget http://freedns.afraid.org/dynamic/check.php -o /dev/null -O /dev/stdout | grep Detected | cut -d : -f 2 | cut -d '<' -
 f 1 | tr -d " ")
- #echo $CurreIP
- if [ "$CurreIP" = "$CacheIP" ]
- then
-   # Both IP are equal
-   echo "Update not required..."
- else
-   # The IP has change
-   echo "Updating http://free.afraid.org with " $CurreIP
-   wget http://freedns.afraid.org/dynamic/update.php?xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -o /dev/null -O /dev/stdout
-   echo `date`  "Updating log with IP " $CurreIP >> dnsactual.log
- fi
- rm -f /etc/freedns/dnsactual.conf
- echo $CurreIP > /etc/freedns/dnsactual.conf
+if [ "$CURRIP" = "$CACHEIP" ]
+then
+  # Both IP are equal
+  echo "Update not required..."
+else
+  # The IP has change
+  echo "Updating http://free.afraid.org with " $CurreIP
+  wget https://freedns.afraid.org/dynamic/update.php?clNFNUVaNnQ1dGVXMFljRzREZ0o6MjQ3NDUwMg== -o /dev/null -O /dev/stdout
+  echo `date`  "Updating log with IP " $CURRIP >> /tmp/$(basename $0).log
+fi
+rm -f $IPFILE
+echo $CURRiP > $IPFILE
