e636f6b4517e3e710d32a213dd84120e8bf7f71f
Kevinesan Pillay <kevin.pillay@is.co.za>
2018-06-28 12:02:12 +0200

    Fixed case for variables

--- dnsactual.sh
+++ dnsactual.sh
@@ -21,17 +21,16 @@ then
   CACHEIP=$(cat $IPFILE)
 fi
 
-CURRIP=$(wget http://freedns.afraid.org/dynamic/check.php -o /dev/null -O /dev/stdout | grep Detected | cut -d : -f 2 | cut -d '<' -
-f 1 | tr -d " ")
+CURRIP=$(wget http://freedns.afraid.org/dynamic/check.php -o /dev/null -O /dev/stdout | grep Detected | cut -d ':' -f 2 | cut -d '<' -f 1 | tr -d " ")
 if [ "$CURRIP" = "$CACHEIP" ]
 then
   # Both IP are equal
   echo "Update not required..."
 else
   # The IP has change
-  echo "Updating http://free.afraid.org with " $CurreIP
+  echo "Updating http://free.afraid.org with " $CURREIP
   wget https://freedns.afraid.org/dynamic/update.php?clNFNUVaNnQ1dGVXMFljRzREZ0o6MjQ3NDUwMg== -o /dev/null -O /dev/stdout
   echo `date`  "Updating log with IP " $CURRIP >> /tmp/$(basename $0).log
 fi
 rm -f $IPFILE
-echo $CURRiP > $IPFILE
+echo $CURRIP > $IPFILE
